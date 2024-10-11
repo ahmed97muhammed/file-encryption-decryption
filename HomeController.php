00a0b7de217605efbee60a1a5d855121c30e3e5c
Ahmed Farrag <86477530+ahmed97muhammed@users.noreply.github.com>
2024-10-11 15:05:46 +0200

    Update HomeController.php

--- HomeController.php
+++ HomeController.php
@@ -44,127 +44,94 @@ class HomeController extends Controller
       //Start Ajax Check
       if(request()->ajax()) 
       {
-
-
-        $validation = Validator::make($request->all(), [
-        'analysis_file' => ['required'],
-
-        ]);
-
-        //Start validation Check
-        if($validation->passes())
-        {
-
-          $file=$request->file('analysis_file'); 
-
-          //forget old values of encrypted_file_content and decrypted_file_content from sessions
-          session()->forget('encrypted_file_content');
-
-          session()->forget('decrypted_file_content');
-
-          $file_contents=file_get_contents($file->getRealPath());
-
-          $encrypted_file=Crypt::encrypt($file_contents);
-
-          $decrypted_file=Crypt::decrypt($encrypted_file);
-
-          //start show file details
-          $file_name_with_ext=$file->getClientOriginalName();
-
-          $file_name=pathinfo($file_name_with_ext, PATHINFO_FILENAME);
-
-          $file_size=$file->getSize();
-          $human_File_Size=$this->humanFileSize($file_size);
-
-          $file_ext=$file->getClientOriginalExtension();
-
-          //Store encrypted_file_content decrypted_file_content in sessions
-
-          Session::put('encrypted_file_content',[$encrypted_file,$file_ext]);
-
-          Session::put('decrypted_file_content',[$decrypted_file,$file_ext]);
-
-          //Return file details to print them into home page
-          return [$file_name,$human_File_Size,$file_ext];
-
-          //End show file details
-
+            $validation = Validator::make($request->all(), [
+            'analysis_file' => ['required'],
+    
+            ]);
+    
+            //Start validation Check
+            if($validation->passes())
+            {
+                  $file=$request->file('analysis_file'); 
+                    
+                  session()->forget('encrypted_file_content');
+                  session()->forget('decrypted_file_content');
+                    
+                  $file_contents=file_get_contents($file->getRealPath());
+        
+                  $encrypted_file=Crypt::encrypt($file_contents);
+        
+                  $decrypted_file=Crypt::decrypt($encrypted_file);
+        
+                  //start show file details
+                  $file_name_with_ext=$file->getClientOriginalName();
+        
+                  $file_name=pathinfo($file_name_with_ext, PATHINFO_FILENAME);
+        
+                  $file_size=$file->getSize();
+                  $human_File_Size=$this->humanFileSize($file_size);
+        
+                  $file_ext=$file->getClientOriginalExtension();
+        
+                  //Store encrypted_file_content decrypted_file_content in sessions
+        
+                  Session::put('encrypted_file_content',[$encrypted_file,$file_ext]);
+        
+                  Session::put('decrypted_file_content',[$decrypted_file,$file_ext]);
+        
+                  return [$file_name,$human_File_Size,$file_ext];
+              }
           }
-
-        //End validation Check
-
+        
       }
-      //End Ajax Check 
-      }
-
-      //Start Function to get humanFileSize
+    
       function humanFileSize($size,$unit="") 
       {
-        if( (!$unit && $size >= 1<<30) || $unit == "GB")
-        return number_format($size/(1<<30),2)."GB";
-        if( (!$unit && $size >= 1<<20) || $unit == "MB")
-        return number_format($size/(1<<20),2)."MB";
-        if( (!$unit && $size >= 1<<10) || $unit == "KB")
-        return number_format($size/(1<<10),2)."KB";
-        return number_format($size)." bytes";
+            if( (!$unit && $size >= 1<<30) || $unit == "GB")
+            return number_format($size/(1<<30),2)."GB";
+            if( (!$unit && $size >= 1<<20) || $unit == "MB")
+            return number_format($size/(1<<20),2)."MB";
+            if( (!$unit && $size >= 1<<10) || $unit == "KB")
+            return number_format($size/(1<<10),2)."KB";
+            return number_format($size)." bytes";
       }
 
-    //Start Function to encrypt  file
       public function file_encrypt_ajax_url(Request $request)
       {
         //Start Ajax Check
         if(request()->ajax()) 
         {
-
-          //save file into storage
-
-          $encrypted_file_content=session()->get('encrypted_file_content')[0];
-
-          $file_ext=session()->get('encrypted_file_content')[1];
-          $headers = array(
-          'Content-Type: application/'.$file_ext,
-          );
-          Storage::disk('local')->put('encrypted.'.$file_ext,$encrypted_file_content,$headers);
-
-          return trans("pages.encrypted");
-
-
+              $encrypted_file_content=session()->get('encrypted_file_content')[0];
+    
+              $file_ext=session()->get('encrypted_file_content')[1];
+              $headers = array(
+              'Content-Type: application/'.$file_ext,
+              );
+              Storage::disk('local')->put('encrypted.'.$file_ext,$encrypted_file_content,$headers);
+    
+              return trans("pages.encrypted");
         }
-
       }
-
-      //Start Function to decrypt a file
+    
       public function file_decrypt_ajax_url(Request $request)
       {
-        //Start Ajax Check
-        if(request()->ajax()) 
-        {
-
-          //save file into storage
-
-          $decrypted_file_content=session()->get('decrypted_file_content')[0];
-          $file_ext=session()->get('decrypted_file_content')[1];
-          $headers = array(
-            'Content-Type: application/'.$file_ext,
-          );
-          Storage::disk('local')->put('decrypted.'.$file_ext,$decrypted_file_content,$headers);
-
-          return trans("pages.decrypted");
-
-
-        }
-
+            if(request()->ajax()) 
+            {
+                  $decrypted_file_content=session()->get('decrypted_file_content')[0];
+                  $file_ext=session()->get('decrypted_file_content')[1];
+                  $headers = array(
+                    'Content-Type: application/'.$file_ext,
+                  );
+                  Storage::disk('local')->put('decrypted.'.$file_ext,$decrypted_file_content,$headers);
+        
+                  return trans("pages.decrypted");
+            }
       }
 
-      //Start Function to download encrypted , decrypted files
       public function download($file,$fname=null)
       {
           ob_clean();  
           return response()->download(storage_path('app/'.$file),$fname);
-
       }
-
-
-
 }
  
